2f4160e9489c81eddcf06f47bf1d8f889e6447d7
i7 <i7@i7B-14>
2024-06-25 12:33:20 +0900

    상수클래스,로그인관련클래스,로고추가

---
+++ TeamCostco/src/main/utils/Constants.java
@@ -0,0 +1,8 @@
+package main.utils;
+
+import java.awt.Color;
+
+public class Constants {
+	public static final Color BUTTON_FOREGROUND_COLOR = new Color(255, 255, 255);
+	public static final Color BUTTON_BACKGROUND_COLOR = new Color(6, 127, 196);
+}
