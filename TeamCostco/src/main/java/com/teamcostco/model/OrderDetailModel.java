76c88135e8cde7a188c13196f2ded0785b3a216f
i7 <i7@i7B-14>
2024-06-28 14:44:22 +0900

    Good

--- TeamCostco/src/main/java/com/teamcostco/model/OrderDetailModel.java
+++ TeamCostco/src/main/java/com/teamcostco/model/OrderDetailModel.java
@@ -6,102 +6,116 @@ import java.sql.SQLException;
 
 public class OrderDetailModel {
 
-	private String client; // 거래처
-	private String item_number; // 품번
-	private String product_name; // 품명
-	private String main_name;
-	private int order_quantity; // 발주수량
-	private int quantity_of_wh; // 입고수량
-	private int remaining_capacity; // 잔량
-	private Date order_date; // 발주일자
+    private int orderRequestId;
+    private String productCode;
+    private String productName;
+    private int orderEmployeeId;
+    private int orderQuantity;
+    private Date requestDate;
+    private String requestStatus;
+    private String clientName;
+    private int quantityOfWh;
 
-	public OrderDetailModel(ResultSet rs) throws SQLException {
+    public OrderDetailModel(ResultSet rs) throws SQLException {
+        this.orderRequestId = rs.getInt("ORDER_REQUEST_ID");
+        this.productCode = rs.getString("PRODUCT_CODE");
+        this.productName = rs.getString("PRODUCT_NAME");
+        this.orderEmployeeId = rs.getInt("ORDEREMPLOYEE_ID");
+        this.orderQuantity = rs.getInt("ORDER_QUANTITY");
+        this.requestDate = rs.getDate("REQUEST_DATE");
+        this.requestStatus = rs.getString("REQUEST_STATUS");
+        this.clientName = rs.getString("CLIENT_NAME");
+        this.quantityOfWh = rs.getInt("QUANTITY_OF_WH");
+    }
 
-		this.client = rs.getString("client_name");
-		this.item_number = rs.getString("product_id");
-		this.product_name = rs.getString("product_name");
-		this.main_name = rs.getString("main_name");
-		this.order_quantity = rs.getInt("order_quantity");
-		this.quantity_of_wh = rs.getInt("quantity_of_wh");
-		this.order_date = rs.getDate("request_date");
+
+    public String getProductName() {
+		return productName;
 	}
 
-	public String getmain_name() {
-		return main_name;
+
+	public void setProductName(String productName) {
+		this.productName = productName;
 	}
 
-	public void setCategory_name(String main_name) {
-		this.main_name = main_name;
-	}
 
-	public String getClient() {
-		return client;
-	}
+	public int getOrderRequestId() {
+        return orderRequestId;
+    }
 
-	public void setClient(String client) {
-		this.client = client;
-	}
+    public void setOrderRequestId(int orderRequestId) {
+        this.orderRequestId = orderRequestId;
+    }
 
-	public String getItem_number() {
-		return item_number;
-	}
+    public String getProductCode() {
+        return productCode;
+    }
 
-	public void setItem_number(String item_number) {
-		this.item_number = item_number;
-	}
+    public void setProductCode(String productCode) {
+        this.productCode = productCode;
+    }
 
-	public String getProduct_name() {
-		return product_name;
-	}
+    public int getOrderEmployeeId() {
+        return orderEmployeeId;
+    }
 
-	public void setProduct_name(String product_name) {
-		this.product_name = product_name;
-	}
+    public void setOrderEmployeeId(int orderEmployeeId) {
+        this.orderEmployeeId = orderEmployeeId;
+    }
 
-	public int getOrder_quantity() {
-		return order_quantity;
-	}
+    public int getOrderQuantity() {
+        return orderQuantity;
+    }
 
-	public void setOrder_quantity(int order_quantity) {
-		this.order_quantity = order_quantity;
-	}
+    public void setOrderQuantity(int orderQuantity) {
+        this.orderQuantity = orderQuantity;
+    }
 
-	public int getQuantity_of_wh() {
-		return quantity_of_wh;
-	}
+    public Date getRequestDate() {
+        return requestDate;
+    }
 
-	public void setQuantity_of_wh(int quantity_of_wh) {
-		this.quantity_of_wh = quantity_of_wh;
-	}
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
 
-	public int getRemaining_capacity() {
-		return remaining_capacity;
-	}
+    public String getRequestStatus() {
+        return requestStatus;
+    }
 
-	public void setRemaining_capacity(int remaining_capacity) {
-		this.remaining_capacity = remaining_capacity;
-	}
+    public void setRequestStatus(String requestStatus) {
+        this.requestStatus = requestStatus;
+    }
 
-	public Date getOrder_date() {
-		return order_date;
-	}
+    public String getClientName() {
+        return clientName;
+    }
 
-	public void setOrder_date(Date order_date) {
-		this.order_date = order_date;
-	}
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
 
-	@Override
-	public String toString() {
-	    StringBuilder sb = new StringBuilder();
-	    String lineSeparator = "\n";
-	    sb.append("거래처=").append(client).append(lineSeparator);
-	    sb.append("발주번호=").append(item_number).append(lineSeparator);
-	    sb.append("상품명=").append(product_name).append(lineSeparator);
-	    sb.append("카테고리=").append(main_name).append(lineSeparator);
-	    sb.append("발주수량=").append(order_quantity).append(lineSeparator);
-	    sb.append("입고수량=").append(quantity_of_wh).append(lineSeparator);
-	    sb.append("발주일자=").append(order_date);
-	    return sb.toString();
-	}
+    public int getQuantityOfWh() {
+        return quantityOfWh;
+    }
 
+    public void setQuantityOfWh(int quantityOfWh) {
+        this.quantityOfWh = quantityOfWh;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        String lineSeparator = "\n";
+        sb.append("발주요청ID=").append(orderRequestId).append(lineSeparator);
+        sb.append("상품코드=").append(productCode).append(lineSeparator);
+        sb.append("상품명=").append(productName).append(lineSeparator);
+        sb.append("발주직원ID=").append(orderEmployeeId).append(lineSeparator);
+        sb.append("발주수량=").append(orderQuantity).append(lineSeparator);
+        sb.append("요청일자=").append(requestDate).append(lineSeparator);
+        sb.append("요청상태=").append(requestStatus).append(lineSeparator);
+        sb.append("거래처이름=").append(clientName).append(lineSeparator);
+        sb.append("입고수량=").append(quantityOfWh);
+        return sb.toString();
+    }
 }
