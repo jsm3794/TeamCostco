3e464c721a5e5d216dcd73ebeeb56889a290e81c
sinbe <sinbe@DESKTOP-Q253FDR>
2024-06-28 14:25:18 +0900

    firstCommit(창고목록 테이블 UI수정)

--- TeamCostco/src/main/java/com/teamcostco/model/ProductTableModel.java
+++ TeamCostco/src/main/java/com/teamcostco/model/ProductTableModel.java
@@ -7,7 +7,7 @@ import javax.swing.table.AbstractTableModel;
 
 public class ProductTableModel extends AbstractTableModel {
     private List<WareHouseListModel> data;
-    private String[] columnNames = {"창고번호", "대분류", "중분류", "소분류", "제품명", "창고수량"};
+    private String[] columnNames = {"상품번호", "제품명", "창고수량", "대분류", "중분류", "소분류"};
 
     public ProductTableModel(List<WareHouseListModel> data) {
         this.data = data;
@@ -30,15 +30,15 @@ public class ProductTableModel extends AbstractTableModel {
             case 0:
                 return row.getProduct_id();
             case 1:
-                return row.getMain_name();
+            	return row.getProduct_name();
             case 2:
-                return row.getMedium_name();
+            	return row.getInventory_amoun();
             case 3:
-                return row.getSmall_name();
+                return row.getMain_name();
             case 4:
-                return row.getProduct_name();
+                return row.getMedium_name();
             case 5:
-                return row.getCurrent_inventory();
+                return row.getSmall_name();
             default:
                 return null;
         }
